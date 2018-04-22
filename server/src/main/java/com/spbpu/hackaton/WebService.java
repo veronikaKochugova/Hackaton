76c022f8481128029e1ad66eb347d40a3b494a75
Veronika <nika-dis@yandex.ru>
2018-04-23 00:42:39 +0300

    create server

---
+++ server/src/main/java/com/spbpu/hackaton/WebService.java
@@ -0,0 +1,14 @@
+package com.spbpu.hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebService {
+    protected WebService() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebService.class, args);
+    }
+}
