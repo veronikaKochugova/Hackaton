752d3fe597f4720144c5225b9e986b2de0b42136
Daria Zelenova <muzzy404@gmail.com>
2018-04-27 00:54:54 +0300

    android client

---
+++ client/app/src/main/java/com/spbpu/hackaton/client/MainActivity.java
@@ -0,0 +1,120 @@
+package com.spbpu.hackaton.client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String SERVER_IP = "192.168.3.234";
+    private static final int SERVER_PORT = 27015;
+
+    String message = new String("");
+
+    EditText editMessage;
+    EditText editIP;
+    Button btnSendMessage;
+    Button btnChangeIP;
+    TextView textMsgFromServer;
+
+    private Socket socket = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editMessage = findViewById(R.id.editMessage);
+        editIP = findViewById(R.id.editServerIP);
+
+        btnSendMessage = findViewById(R.id.btnSendMessage);
+        btnChangeIP = findViewById(R.id.btnChangeServerIP);
+
+        textMsgFromServer = findViewById(R.id.txtMessage);
+
+        btnSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message = editMessage.getText().toString();
+                editMessage.setText("");
+
+                if (socket == null) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            openConnection();
+                        }
+                    }).start();
+                }
+
+                try {
+                    sentData(message.getBytes());
+                } catch (Exception e) {
+                    textMsgFromServer.setText("Sending message error");
+                }
+            }
+        });
+
+        btnChangeIP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeConnection();
+                SERVER_IP = editIP.getText().toString();
+                editIP.setText("");
+                socket = null;
+            }
+        });
+    }
+
+    public void openConnection() {
+        closeConnection();
+
+        try {
+            InetAddress serverAdr = InetAddress.getByName(SERVER_IP);
+            Log.d("Dasha", serverAdr.toString());
+            socket = new Socket(serverAdr, SERVER_PORT);
+        } catch (UnknownHostException e) {
+            Log.d("Dasha", e.getMessage());
+        } catch (IOException e) {
+            Log.d("Dasha", e.getMessage());
+        }
+    }
+
+    public void closeConnection() {
+        if (socket != null && !socket.isClosed()) {
+            try {
+                socket.close();
+            } catch (Exception e) {
+                textMsgFromServer.setText("Unable close socket");
+            }
+        }
+    }
+
+    public void sentData(byte[] data) {
+        if (socket == null || socket.isClosed()) {
+            return;
+        }
+
+        try {
+            socket.getOutputStream().write(data);
+            socket.getOutputStream().flush();
+        } catch (IOException e) {
+            textMsgFromServer.setText("Unable to sent message (IOException)");
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        closeConnection();
+    }
+}
