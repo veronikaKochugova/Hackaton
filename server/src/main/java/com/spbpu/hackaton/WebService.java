c4673bf360ec2f33417db7b65bcb004acf00c227
Daria Zelenova <muzzy404@gmail.com>
2018-04-27 20:08:20 +0300

    android http client

--- server/src/main/java/com/spbpu/hackaton/WebService.java
+++ server/src/main/java/com/spbpu/hackaton/WebService.java
@@ -2,8 +2,11 @@ package com.spbpu.hackaton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class WebService {
     protected WebService() {
     }
