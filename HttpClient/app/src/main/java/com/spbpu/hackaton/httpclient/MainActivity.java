ed3a86dd7ff79fb68a7a925c8a04e91a5a7b71a9
nika <nika-dis@yandex.ru>
2018-04-29 03:39:59 +0300

    added parsing data for pieChart and putting data to HashMap<>

--- HttpClient/app/src/main/java/com/spbpu/hackaton/httpclient/MainActivity.java
+++ HttpClient/app/src/main/java/com/spbpu/hackaton/httpclient/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -22,18 +23,25 @@ import org.json.JSONObject;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     static private final String URL_GET_ALL = "allCountries";
+    static private final String URL_GET_YEARS = "years";
+    static private final String URL_GET_PIE = "pie";
     //static private final String URL = "http://192.168.3.234:8090/";
     static private final String URL = "http://10.20.0.95:8090/";
 
     TextView txtMessage;
     Button btnGet;
     String[] allCountries;
+    String[] years;
+    String[] pieData;
+    Map<String,String> pieDataMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,40 +59,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
 
         switch (view.getId()) {
-            // JSONObject request
-            /*case R.id.btnGet:
-
-                JsonObjectRequest getRequest = new JsonObjectRequest(
-                        Request.Method.GET,
-                        URL_GET, null,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                txtMessage.setText(response.toString());
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                txtMessage.setText("ERROR: " + error.toString());
-                            }
-                        });
-                RequestQueue requestQueue = Volley.newRequestQueue(this);
-                requestQueue.add(getRequest);
-
-                break;*/
+//            // JSONObject request
+//            /*case R.id.btnGet:
+//
+//                JsonObjectRequest getRequest = new JsonObjectRequest(
+//                        Request.Method.GET,
+//                        URL_GET, null,
+//                        new Response.Listener<JSONObject>() {
+//                            @Override
+//                            public void onResponse(JSONObject response) {
+//                                txtMessage.setText(response.toString());
+//                            }
+//                        },
+//                        new Response.ErrorListener() {
+//                            @Override
+//                            public void onErrorResponse(VolleyError error) {
+//                                txtMessage.setText("ERROR: " + error.toString());
+//                            }
+//                        });
+//                RequestQueue requestQueue = Volley.newRequestQueue(this);
+//                requestQueue.add(getRequest);
+//
+//                break;*/
 
             // String request
             case R.id.btnGet:
                 StringRequest getRequest = new StringRequest(
                         Request.Method.GET,
-                        URL + URL_GET_ALL,
+                        URL + URL_GET_PIE + "?country=Angola&year=1900",
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
                                 txtMessage.setText("OK : ");
                                 String tmp = response;
-                                allCountries = tmp.split(",");
+                                pieData = tmp.replaceAll("\\{\"|\"\\}", "")
+                                        .split("\",\"");
+                                for(String item : pieData){
+                                    String str[] = item.split("\":\"");
+                                    pieDataMap.put(str[0], str[1]);
+                                }
+                                Log.d("<NIKA>", String.valueOf(pieDataMap));
                             }
                         },
                         new Response.ErrorListener() {
